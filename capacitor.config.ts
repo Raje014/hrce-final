36e4a1682e9118c0f8539145c1b73ea391d30f39
raje014 <rajalakshmirajesh1422@gmail.com>
2026-03-05 10:32:02 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'temple-map',
+  webDir: 'www'
+};
+
+export default config;
